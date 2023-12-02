49e0576ecce415095fd72c887d796848ea4206f0
saurabh-gih <saurav999bsr@gmail.com>
2023-12-02 14:13:15 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Navigation from './Components/Navigation'
+
+function App() {
+
+  return (
+    <>
+      <nav>
+        <button>Sign Up</button>
+      </nav>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Navigation.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+const Navigation = () => {
+  const [collapse, setCollapse] = useState(false);
+
+  return (
+    <header className="primary-header flex">
+      
+    </header>
+  );
+};
+
+export default Navigation;
